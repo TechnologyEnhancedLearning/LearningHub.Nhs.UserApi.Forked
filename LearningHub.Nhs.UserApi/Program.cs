cb03bf16db35cf6d967273b7c5204c15a77097c3
Swapnamol Abraham <Swapnamol.Abraham@hee.nhs.uk>
2025-03-19 11:11:09 +0000

    reverted

--- LearningHub.Nhs.UserApi/Program.cs
+++ LearningHub.Nhs.UserApi/Program.cs
@@ -10,7 +10,6 @@ using NLog.Web;
 
 var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-var csp = "object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts allow-popups; base-uri 'self';";
 try
 {
     logger.Debug("Log Started");
