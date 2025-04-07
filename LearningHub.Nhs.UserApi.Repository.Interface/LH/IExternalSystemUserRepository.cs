429e9a44bd512a93c2a45b861025821957cfc808
Swapnamol Abraham <Swapnamol.Abraham@hee.nhs.uk>
2025-04-07 17:16:32 +0100

    TD-5407: Implemented a fix to avoid SQL exception on LinkExistingUserToSso method

--- LearningHub.Nhs.UserApi.Repository.Interface/LH/IExternalSystemUserRepository.cs
+++ LearningHub.Nhs.UserApi.Repository.Interface/LH/IExternalSystemUserRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace LearningHub.Nhs.UserApi.Repository.Interface.LH
 {
     using System.Threading.Tasks;
+    using elfhHub.Nhs.Models.Entities;
     using LearningHub.Nhs.Models.Entities.External;
 
     /// <summary>
@@ -15,5 +16,12 @@
         /// <param name="externalSystemId">The external system id.</param>
         /// <returns>The <see cref="ExternalSystemUser"/>.</returns>
         Task<ExternalSystemUser> GetByIdAsync(int userId, int externalSystemId);
+
+        /// <summary>
+        /// Create External system user.
+        /// </summary>
+        /// <param name="userExternalSystem">The userExternalSystem.</param>
+        /// <returns>The <see cref="ExternalSystemUser"/>.</returns>
+        Task CreateExternalSystemUserAsync(ExternalSystemUser userExternalSystem);
     }
 }
